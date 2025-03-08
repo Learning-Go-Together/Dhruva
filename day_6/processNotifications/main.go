b060f56f7a478f9b421a00c26e6f191f8467bf34
Dhruva <rshivang12345@gmail.com>
2025-03-09 00:10:42 +0530

    complete interface

--- day_6/processNotifications/main.go
+++ day_6/processNotifications/main.go
@@ -22,8 +22,31 @@ type systemAlert struct {
 	messageContent string
 }
 
-// ?
+
+func (d directMessage) importance() int {
+	if d.isUrgent {
+		return 50
+	}
+	return d.priorityLevel
+}
+
+func (g groupMessage) importance() int {
+	return g.priorityLevel
+}
+
+
+func (s systemAlert) importance() int {
+	return 100
+}
 
 func processNotification(n notification) (string, int) {
-	// ?
+	if dm, ok := n.(directMessage); ok {
+		return dm.senderUsername, dm.importance()
+	} else if gm, ok := n.(groupMessage); ok {
+		return gm.groupName, gm.importance()
+	} else if sa, ok := n.(systemAlert); ok {
+		return sa.alertCode, sa.importance()
+	}
+	
+	return "", 0
 }
